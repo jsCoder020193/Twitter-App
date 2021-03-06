fe81f000cb6359f9603e7dc41a146cb2ca24c0c7
Subba <Subba@gmail.com>
2021-03-06 00:28:57 -0600

    Assignment 3 Submission

---
+++ Twitter/tweetCell.swift
@@ -0,0 +1,28 @@
+//
+//  tweetCell.swift
+//  Twitter
+//
+//  Created by SubbaLakshmi on 3/5/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class tweetCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
